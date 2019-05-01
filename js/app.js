7852f6df6531d8f0f4c9e3df97a84b43350c0529
Eric Tovar <tovareric@gmail.com>
2019-05-01 17:23:38 -0500

    finishing user stories

--- js/app.js
+++ js/app.js
@@ -4,13 +4,31 @@
 // we need to add functianlity to the button so that when we click we get the value
 // we will build a dom element using that value
 // then we will add that element to the DOM
-
+// add a delete button -span tag with x as its content
+// add function that will pluck it from the DOM
 
 // State Variables
-let $inputVal 
+let $inputVal , $skillElement
 // Event Listeners
 $('button').on('click', getInputText)
 //Functions
 function getInputText () {
-    alert('Button Clicked')
+    $inputVal = $('input').val()
+    $('input').val('');
+    createElement($inputVal)
+}
+
+function createElement(str) {
+    $skillElement = $(`<li><span>x</span> ${str}</li>`)
+    addSkill($skillElement)
+}
+
+function addSkill(element) {
+$('section ul').append(element);
+$('span').on('click', removeSkill)
+}
+
+function removeSkill(evt){
+    $(evt.target).parent().remove();
+    
 }
