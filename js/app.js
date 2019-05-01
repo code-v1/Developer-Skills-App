1d21ff03ad999d799bf78c85b0d8fe840b6d505a
Eric Tovar <tovareric@gmail.com>
2019-05-01 16:50:35 -0500

    adding preliminary css and jQuery

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// we're going to make a Dev Skilss App
+// we need for a user to type in a dev skill
+// then we will get the value of the skill
+// we need to add functianlity to the button so that when we click we get the value
+// we will build a dom element using that value
+// then we will add that element to the DOM
+
+
+// State Variables
+let $inputVal 
+// Event Listeners
+$('button').on('click', getInputText)
+//Functions
+function getInputText () {
+    alert('Button Clicked')
+}
